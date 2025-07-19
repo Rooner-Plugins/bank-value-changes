a5126b86c03fd9aff62e789c6579cd1a92d8376c
Rooner <moveslikemcgee@gmail.com>
2025-07-19 17:50:32 +0300

    refactor names and add plugin properties

--- src/test/java/com/bankvaluechanges/BankValueChangesTest.java
+++ src/test/java/com/bankvaluechanges/BankValueChangesTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.bankvaluechanges;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class BankValueChangesTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(BankValueChangesPlugin.class);
 		RuneLite.main(args);
 	}
 }
